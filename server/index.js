2d9d1eb059685199fde0b8080809761759bf8a4f
unknown <victoradelowo@yahoo.com>
2019-08-09 05:08:41 +0100

    debuged express static;

--- server/index.js
+++ server/index.js
@@ -25,9 +25,11 @@ authRoutes(app);
 billingRoutes(app);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static['client/build']);
+    
+    app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res)=>{
+        
         res.sendFile( path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
